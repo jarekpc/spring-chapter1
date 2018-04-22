0841fb87675cc68e18fd0fd1376e0b1a12a8f9fa
Jarek Zyzak <jarekpc@o2.pl>
2018-04-22 22:08:57 +0200

    first commit for cloud

---
+++ User.java
@@ -0,0 +1,16 @@
+package com.example.springchapter1;
+
+public class User {
+
+    private int id;
+    private String email;
+    private String password;
+
+    private int age;
+    private int gender;
+    private int role;
+
+
+
+
+}
