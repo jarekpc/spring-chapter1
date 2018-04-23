46367553d1bf7eb5dbb9b17cc0fc54d20a4895ba
Jarek Zyzak <jarekpc@o2.pl>
2018-04-23 21:35:54 +0200

    added entity User and properties with dockefile

---
+++ src/main/java/com/example/springchapter1/User.java
@@ -0,0 +1,14 @@
+package com.example.springchapter1;
+
+public class User {
+
+    private int id;
+    private String email;
+    private String password;
+
+    private int age;
+    private int gender;
+    private int role;
+
+
+}
