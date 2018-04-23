48c9bdcb95c0d86d15abc89a462e9c4009f2181d
Jarek Zyzak <jarekpc@o2.pl>
2018-04-23 21:32:06 +0200

    initial project

---
+++ src/main/java/com/example/springchapter1/SpringChapter1Application.java
@@ -0,0 +1,12 @@
+package com.example.springchapter1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChapter1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringChapter1Application.class, args);
+	}
+}
